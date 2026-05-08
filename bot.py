787ebe5d3f7b127dd786dd692033130bba2f5bdb
kalli <rhuanqueirozdasilva2009@gmail.com>
2026-05-08 11:13:45 -0300

    add flask

--- bot.py
+++ bot.py
@@ -238,5 +238,22 @@ async def painel(ctx):
         embed=embed,
         view=Painel()
     )
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot online!"
+
+def run_web():
+    app.run(host="0.0.0.0", port=10000)
+
+def keep_alive():
+    t = Thread(target=run_web)
+    t.start()
+
+keep_alive()
 
 bot.run(TOKEN)
